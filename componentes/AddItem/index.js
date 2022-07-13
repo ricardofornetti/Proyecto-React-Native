747ec7cc8eda69610c2a9edd403ada9d593943a6
RicardoFornetti <ricardofornetti@hotmail.com.ar>
2022-07-13 16:52:18 -0300

    fix error

--- componentes/AddItem/index.js
+++ componentes/AddItem/index.js
@@ -1,33 +1,37 @@
 
-import { View, TextInput, Button, StyleSheet } from "react-native"
+import { View, TextInput, Button, StyleSheet } from "react-native";
 
-export default function AddItem (props){
-    const{onHandlerChangeItem, onHandlerAddItem, textItem} =(props)
+export default function AddItem(props) {
+  const { onHandlerChangeItem, onHandlerAddItem, textItem } = props;
 
-    return(
-        <View style={styles.container}>
-            <TextInput 
-            placeholder='Escribe Aqui' 
-            style={styles.input} 
-            value={textItem}
-            onChangeText={onHandlerChangeItem} >
-            </TextInput>
-            <Button title='Agregar' onPress={onHandlerAddItem} disabled={textItem.length < 1 ? true :false}></Button>
-        </View>
-    )
+  return (
+    <View style={styles.container}>
+      <TextInput
+        placeholder="Escribe Aqui"
+        style={styles.input}
+        value={textItem}
+        onChangeText={onHandlerChangeItem}
+      />
+      <Button
+        title="Agregar"
+        onPress={onHandlerAddItem}
+        disabled={textItem.length < 1 ? true : false}
+      />
+    </View>
+  );
 }
 
 const styles = StyleSheet.create({
+
     container: {
-        alignItems: 'center',
-        justifyContent: 'space-between',
-        flexDirection: 'row'  
+        alignItems: "center",
+        justifyContent: "space-between",
+        flexDirection: "row",
     },
     input: {
-        width: '80%',
-        height: 50,
-        borderColor: 'black',
-        borderWidth: 1,
+    width: "80%",
+    height: 50,
+    borderColor: "black",
+    borderWidth: 1,
     },
-})
-
+});
